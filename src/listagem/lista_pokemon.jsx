09655e9d03fb8c2a47bbf77bfdbdc0cb390d3f37
samuel0811 <samuelresendegarcia@gmail.com>
2024-05-06 08:28:08 -0300

    pokemon listagem

---
+++ src/listagem/lista_pokemon.jsx
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from "react";
+
+const listagem = () => (
+    <div>
+        <ul>
+            <li>Pokemón 1</li>
+            <li>Pokemón 2</li>
+            <li>Pokemón 3</li>
+            <li>Pokemón 4</li>
+            <li>Pokemón 5</li>
+        </ul>
+    </div>
+);
+
+export default Listagem 
